5a8e13f720b72f7583c7fdec0401a1c323f26aaf
henpch <148641891+henpch@users.noreply.github.com>
2023-10-21 12:48:53 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt install curl
 #echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install nala snapd neovim code brave-browser easytag gnome-tweaks htop copyq flameshot simplescreenrecorder gparted vlc vim kdenlive audacity git curl neofetch lutris -y
-flatpak install spotify pycharm
+flatpak install spotify grapejuice
 flatpak install flathub org.onlyoffice.desktopeditors it.mijorus.gearlever com.mattjakeman.ExtensionManager net.ankiweb.Anki org.qbittorrent.qBittorrent com.raggesilver.BlackBox com.jetbrains.PyCharm-Community io.github.flattool.Warehouse org.gnome.Boxes
 sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
